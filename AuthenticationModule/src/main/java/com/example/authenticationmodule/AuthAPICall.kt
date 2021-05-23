41d803cecdfeff512eeccfa97b33859102868d2e
Vidhey <manoj@in.arokiait.com>
2021-05-23 20:05:37 +0530

    Token access implemented

--- AuthenticationModule/src/main/java/com/example/authenticationmodule/AuthAPICall.kt
+++ AuthenticationModule/src/main/java/com/example/authenticationmodule/AuthAPICall.kt
@@ -24,8 +24,9 @@ object AuthAPICall {
         interceptor.level = HttpLoggingInterceptor.Level.BASIC
         interceptor.level = HttpLoggingInterceptor.Level.HEADERS
         interceptor.level = HttpLoggingInterceptor.Level.BODY
+        clients.addInterceptor(interceptor)
 
-        val client: OkHttpClient = clients.addInterceptor(interceptor).build()
+        val client: OkHttpClient = clients.build()
 
         val mediaType = "application/x-www-form-urlencoded".toMediaTypeOrNull()
         val body: RequestBody = requestBody.toRequestBody(mediaType)
@@ -46,18 +47,19 @@ object AuthAPICall {
 
             override fun onResponse(call: Call, response: Response) {
                 if (response.isSuccessful) {
-                    val string  = "{\"access_token\":\"5ZR8CxXDdsDJlBRpoWMCl3Tk9n-BOqUuovXL4VIzVk1A7RQXC1A-luODxZzN2c-h7PBgl9VdMo_uTsar_EZXk7Ojg86-kXYMJgmWMAAm7D8b1pcis3KIQpfMKEIQwWDmdodK6rn0RuC61G1ClLbvsOVb5m6Pj--_-52hf8OR99YCZqnbOY8F83TiGldPpFmY-sB14hdV6cRHseiEq2pHHKHwttCEVvemsRLTiQgfRoo\",\"token_type\":\"bearer\",\"expires_in\":14399,\"userName\":\"partnerrewards\",\".issued\":\"Thu, 20 May 2021 16:22:09 GMT\",\".expires\":\"Thu, 20 May 2021 20:22:09 GMT\"}"
 
-                    val token_data = Moshi.Builder().add(KotlinJsonAdapterFactory()).build()
+                    val token_data = Moshi.Builder()
+                            .add(KotlinJsonAdapterFactory())
+                            .build()
                             .adapter(Token::class.java)
-                            .fromJson(string)
-//                            .fromJson(response.body.toString())
+                            .fromJson(response.body?.source()?.buffer)
 
                     PreferenceHelperToken.setTokenDetails(context, token_data!!)
 
                     Log.d("TAG", "onResponse: ${token_data.access_token}")
 
                     preferenceTokenCallBack.tokenDetails(token_data)
+
                 }
             }
         })
